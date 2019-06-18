349039bba3e9a407bb16c5c0c5e45e876406bd4f
rio <kaihatukogo@gmail.com>
2019-06-18 18:37:06 +0900

    users_controller.rb changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,13 +60,15 @@ class UsersController < ApplicationController
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
 
        # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
     
       def admin_user
         redirect_to(root_url) unless current_user.admin?
       end
-end
+ end
